910ba46647b902ab4a5bcb47836cf28c0b4df664
powotato <30554162+1OwO1@users.noreply.github.com>
2024-12-18 22:17:00 +0800

    merge

--- countPath/server.py
+++ countPath/server.py
@@ -25,16 +25,56 @@ def dest():
 def getPos():
     print("call getPos")
     # pos.recalibrate()
+    # pos.fake_read()     # if you don't have UWB module, use this
+    pos.UWB_read()      # if you have UWB module, use this
+    # x, y = pos.compute_CRS()
+    x, y = pos.UWB_read_compute_CRS_5()
+    # x, y = pos.get_anchor_CRS('9')
+    print(f"coordinate: {y}, {x}")
+    return jsonify([x, y]), 200
+
+
+@app.route('/pos/anchor/<anchor_number>')
+def getAnchorPos(anchor_number):
+    x, y = pos.get_anchor_CRS(anchor_number)
+    return jsonify([x, y]), 200
+
+
+@app.route('/pos/recalibrate')
+def recalibrate():
+    x, y = pos.recalibrate()
+    return jsonify([x, y]), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500, debug=True)
+
+app = Flask(__name__)
+CORS(app)
+pos = UWBpos()
+# pos.recalibrate()
+
+
+@app.route('/dest', methods=['POST'])
+def dest():
+    print('Destination received')
+    print(request.json['dest'])
+
+    route = findRoute(request.json['st'], request.json['dest'])
+    response_data = {
+        'route': route
+    }
+    return jsonify(response_data), 200
+
+
+@app.route('/pos')
+def getPos():
+    # pos.recalibrate()
     pos.fake_read()     # if you don't have UWB module, use this
     # pos.UWB_read()      # if you have UWB module, use this
     # x, y = pos.compute_CRS()
-<<<<<<< HEAD
     # x, y = pos.UWB_read_compute_CRS_5()
     # x, y = pos.get_anchor_CRS('6')
-=======
-    x, y = pos.UWB_read_compute_CRS_5()
-    # x, y = pos.get_anchor_CRS('9')
->>>>>>> 96168381f11b2a28ebff30c7bba116713e91d498
     print(f"coordinate: {y}, {x}")
     return jsonify([x, y]), 200
 
