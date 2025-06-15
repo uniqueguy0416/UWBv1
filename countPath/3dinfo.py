78110d55c4f795cc576992a56576a3d6aa7a3e8d
uniqueguy0416 <k408440096@gmail.com>
2025-06-15 08:05:53 +0800

    1

--- countPath/3dinfo.py
+++ countPath/3dinfo.py
@@ -28,10 +28,10 @@ anchor_ids = [
 ]
 anchor_labels = ['anchor6', 'anchor7', 'anchor8', 'anchor9']
 anchor_positions = [
-    (0.00, 4.00, 1.00),
+    (3.00, 0.00, 1.5),
     (0.00, 0.00, 0.00),
     (1.00, 0.00, 0.5),
-    (0.00, 2.00, 0.5)
+    (2.00, 0.00, 0.5)
 ]
 faulty_id = '0341000000000000'
 
@@ -152,8 +152,8 @@ def main():
     # 輸出目錄與檔案路徑
     output_dir = os.path.expanduser('/home/e520/uwb_results')
     os.makedirs(output_dir, exist_ok=True)
-    csv_path = os.path.join(output_dir, 'uwb_偏側_含估計座標.csv')
-    xlsx_path = os.path.join(output_dir, 'uwb_偏側偏側_含估計座標.xlsx')
+    csv_path = os.path.join(output_dir, 'uwb_線性線性_含估計座標.csv')
+    xlsx_path = os.path.join(output_dir, 'uwb_線性_含估計座標.xlsx')
 
     # 儲存 CSV & Excel
     df.to_csv(csv_path, index=False)
